6b84d3e0c81b9228eb8b56173ec831b7dd86911c
jackpadilla <pp_pp92@hotmail.com>
2016-03-31 13:03:22 -0600

    reportes view y ruta

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,4 @@
+class StaticPagesController < ApplicationController
+  def reporte
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'static_pages/reporte'
+
+  get 'static_pages/Contacto'
+
   devise_for :admins
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
@@ -9,7 +13,7 @@ Rails.application.routes.draw do
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
-
+    get 'reporte'=>'static_pages#reporte'
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
