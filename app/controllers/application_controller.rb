113f4e5ac16f332a454fce98a7af697642f1d0b8
Andres Lopez De Leon <agldeleon@gmail.com>
2016-04-20 14:03:50 -0500

    Add user form after registration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,16 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def after_sign_in_path_for(resource)
+  	if current_user.nil?
+  		'/admin/home'
+	else
+	  if current_user.street == nil
+	  	'/user/form'
+	  else
+	  	'/home'
+	  end
+	end
+  end
 end
