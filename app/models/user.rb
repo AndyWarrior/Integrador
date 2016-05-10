42a226b3a703ddb6b96e320e22ceae2bfc3f3830
Andres Lopez De Leon <agldeleon@gmail.com>
2016-05-10 15:45:48 -0500

    Add more fields to csv report

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,14 +6,24 @@ class User < ActiveRecord::Base
          :timeoutable
 
   def self.to_csv
-    attributes = %w{id email nombre apellido}
+    attributes = %w{id email nombre apellido programa 
+      proyecto calle colonia ciudad estado c.p. telefono 
+      habitantes cuartos paredes techo piso albañil 
+      tienda_cercana}
 
     CSV.generate(headers: true) do |csv|
       csv << attributes
 
       all.each do |user|
-        csv << [user.id, user.email, user.first_name, user.last_name]
+        csv << [user.id, user.email, user.first_name, 
+          user.last_name, Program.find(user.program).name, 
+          Project.find(user.project).name, user.street, 
+          user.neighborhood, user.city, user.state, 
+          user.zip_code, user.telephone, user.people_living, 
+          user.rooms, user.wall_material, user.roof_material, 
+          user.floor_material, user.build_yourself, user.store]
       end
     end
   end
+
 end
